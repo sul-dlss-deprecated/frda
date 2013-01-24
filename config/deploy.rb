8eb39af7af0087016a0e8fd4ec1113876fd8364d
Peter Mangiafico <peter@mangiafico.org>
2013-01-23 17:06:41 -0800

    update deployment scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,7 +74,7 @@ namespace :db do
     run "cd #{deploy_to}/current && rake db:fixtures:load RAILS_ENV=#{rails_env}"
   end
   task :symlink_sqlite do
-    run "ln -s #{shared_path}/#{rails_env}.sqlite3 #{release_path}/db/#{rails_env}.sqlite3"
+    run "ln -fs #{shared_path}/#{rails_env}.sqlite3 #{release_path}/db/#{rails_env}.sqlite3"
   end  
 end
 

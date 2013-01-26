51f0417d77e3dd9d322e81b7e573cc521436eb4f
Jessie Keck <jessie.keck@gmail.com>
2013-01-25 17:26:52 -0800

    Override some blacklight stuff so we can use i18n keys as labels in config.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,4 +1,9 @@
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
-
+  
+  def render_index_field_label args
+    field = args[:field]
+    html_escape("#{t(index_fields[field].label)}:")
+  end
+  
 end

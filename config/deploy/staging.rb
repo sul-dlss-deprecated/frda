9210f7b536627c3cc95bcc61c5008da53139f391
Jessie Keck <jessie.keck@gmail.com>
2013-02-28 12:27:42 -0800

    Update cap deploy scripts to run db:seed after db:migrate.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,5 +6,4 @@ role :web, deployment_host
 role :app, deployment_host
 role :db,  deployment_host, :primary => true
 
-after "deploy:finalize_update", "db:symlink_sqlite"
-after "deploy:create_symlink", "db:loadseeds"
+after "db:migrate", "db:loadseeds"

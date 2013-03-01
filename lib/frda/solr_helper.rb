b9070b334780711ac1552c6fd14bf0ab30e19906
Jessie Keck <jessie.keck@gmail.com>
2013-02-28 16:50:58 -0800

    Add comment about ways to change our some of the solr_helper codein future versions of Blacklight.

--- lib/frda/solr_helper.rb
+++ lib/frda/solr_helper.rb
@@ -5,6 +5,9 @@ module Frda::SolrHelper
     merged_params = self.solr_search_params(user_params).merge(extra_controller_params.merge(group_results_params))
     rows = params.dup.delete(:per_page) || blacklight_config.default_solr_params[:rows] # will we need rows later?
 
+    # After Blacklight upgrade change to:
+    # response = blacklight_solr.send_and_receive(blacklight_config.solr_path, :params=>merged_params)
+    # and remove the rows line above
     response = blacklight_solr.paginate(params[:page] || 1, rows, blacklight_config.solr_path, :params=>merged_params)
     solr_response = Frda::GroupedSolrResponse.new(force_to_utf8(response), merged_params)
     

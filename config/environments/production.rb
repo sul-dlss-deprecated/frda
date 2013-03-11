4c3cd0bff9bffca717d52efd28c007e16e91464e
Peter Mangiafico <peter@mangiafico.org>
2013-03-11 14:02:23 -0700

    add google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,7 @@ Frda::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  GA.tracker = "UA-7219229-19" # frda
+  
 end

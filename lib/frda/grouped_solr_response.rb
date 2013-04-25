6130ae4a8dbda5a87504ecff858220ac020226ad
Jessie Keck <jessie.keck@gmail.com>
2013-04-24 20:40:47 -0700

    Add SolrResponseTermFrequency module to GroupedSolrResponse and Blacklight::SolrResponse.

--- lib/frda/grouped_solr_response.rb
+++ lib/frda/grouped_solr_response.rb
@@ -1,7 +1,10 @@
 require "blacklight/mash" unless defined?(Mash)
+require "solr_response_term_frequencies"
 
 class Frda::GroupedSolrResponse < Mash
   
+  include SolrResponseTermFrequencies
+
   attr_reader :request_params
   def initialize(data, request_params)
     super(data)
@@ -9,7 +12,7 @@ class Frda::GroupedSolrResponse < Mash
     
     extend Blacklight::SolrResponse::Facets
   end
-  
+
   class SolrGroup
     attr_reader :group, :total, :start, :docs
     def initialize group, total, start, docs, response
@@ -27,15 +30,15 @@ class Frda::GroupedSolrResponse < Mash
   def total
     group_element["ngroups"].to_s.to_i
   end
-  
+
   def total_docs
     group_element["matches"].to_s.to_i
   end
-  
+
   def docs
     groups
   end
-  
+
   def start
     params["start"].to_s.to_i
   end
@@ -47,12 +50,12 @@ class Frda::GroupedSolrResponse < Mash
   def params
     (header and header['params']) ? header['params'] : request_params
   end
-  
+
   def rows
     # better way to get rows?  params['rows'] seems to be an array sometimes.
     [request_params["rows"], params["rows"]].flatten.delete_if{|r| r.to_i == 0}.first.to_s.to_i
   end
-    
+
   def groups
     @groups ||= group_element["groups"].map do |group|
       SolrGroup.new(group["groupValue"], group["doclist"]["numFound"], group["doclist"]["start"], group["doclist"]["docs"], self) 

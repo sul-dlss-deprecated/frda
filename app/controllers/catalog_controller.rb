47dfa85158b3ff7bb31b3df9ee56aaf8dd988a06
Jessie Keck <jessie.keck@gmail.com>
2013-12-19 15:49:43 -0800

    Strip out quotes from user query parameter when doing an 'In Speeches By' type search.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -413,7 +413,7 @@ class CatalogController < ApplicationController
 
   def search_within_speeches(solr_params, user_params)
     unless user_params["speeches"].blank? and user_params["by-speaker"].blank?
-      solr_params[:q] = "spoken_text_ftsimv:\"aaa#{user_params['by-speaker']}zzz #{user_params['q']}\"~10000"
+      solr_params[:q] = "spoken_text_ftsimv:\"aaa#{user_params['by-speaker']}zzz #{user_params['q'].gsub('"','')}\"~10050"
       solr_params[:defType] = "lucene"
     end
   end

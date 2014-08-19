216d02ec298edd6d340e3734e3f8c71fe858ba96
Chris Beer <cabeer@stanford.edu>
2014-08-19 13:35:26 -0700

    Exclude staging group when deploying to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,5 @@ server 'frda-prod.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "production"
+
+set :bundle_without, %w{development test staging}.join(' ')  

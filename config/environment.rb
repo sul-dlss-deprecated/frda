64abd39b9d536979a15ee385873da0f35a8392c4
Chris Beer <cabeer@stanford.edu>
2015-10-27 15:51:09 -0700

    Remove image-viewer references

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Frda::Application.initialize!
 
-Frda::Application.config.purl_plugin_server = "prod"
-Frda::Application.config.purl_plugin_location = "//image-viewer.stanford.edu/assets/purl_embed_jquery_plugin.js"
 Frda::Application.config.purl = "http://purl.stanford.edu"

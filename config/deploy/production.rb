dd40e5f7c937a613cc9b05ab3bae412ae74705ac
Anthony Zanella <azanella@stanford.edu>
2016-01-25 13:17:20 -0800

    Cap changes and update to mysql2 for deploy to new vms

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-server 'frda-prod.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
+server 'sul-frda-prod.stanford.edu', user: 'frda', roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, "production"
 
-set :bundle_without, %w{development test staging}.join(' ')  
+set :bundle_without, %w{development test staging}.join(' ')
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-server 'frda-stage.stanford.edu', user: 'lyberadmin', roles: %w{web db app}
+server 'sul-frda-stage.stanford.edu', user: 'frda', roles: %w{web db app}
 
 Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "staging"
+set :rails_env, "production"

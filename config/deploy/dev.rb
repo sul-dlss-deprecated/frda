5b9ff4fc61cfb87afd6d1b0a91b3aa1eab2e4a53
Jack Reed <phillipjreed@gmail.com>
2018-08-07 08:37:16 -0600

    remove pending decommission env

--- config/deploy/dev.rb
+++
@@ -1,8 +0,0 @@
-server 'sul-frda-dev.stanford.edu', user: 'frda', roles: %w{web db app}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "development"
-
-set :assets_roles, [:none]
-
-set :bundle_without, %w{production staging}.join(' ')
